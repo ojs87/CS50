9f6b67206c74ed420c1673f90e6f538d39727e2f
ojs87 <73496902+ojs87@users.noreply.github.com>
2020-11-16 09:20:10 +0000

    Project 1 - Wiki

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.entry, name="entry"),
+    path("createnewpage.html", views.newpage, name="newpage"),
+    path("wiki/<str:name>/edit", views.editpage, name="editpage"),
+    path("wiki/", views.randompage, name="randompage")
 ]
